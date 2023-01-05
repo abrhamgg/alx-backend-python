e45d92853cd55c4584e2fad77d1df231198c72f6
abrhamgg <abrhamgg@gmail.com>
2023-01-05 10:21:45 +0300

    1-concat.py completed

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/ev python3
+
+"""concat string"""
+
+def concat(str1: str, str2: str) -> str:
+    """cocnat -> function that concatenates two variables
+
+    Args:
+        str1 (str): string 1
+        str2 (str): string 2
+
+    Returns:
+        str: srting 1 + string 2
+    """
+    return str1 + str2
