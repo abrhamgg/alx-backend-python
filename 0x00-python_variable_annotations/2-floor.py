c2f7052077344668f0511b8791601cb71a3dd027
abrhamgg <abrhamgg@gmail.com>
2023-01-05 12:21:28 +0300

    hotfix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-import math
-"""floor module"""
+'''Task 2's module.
+'''
 
 
-def floor(n: float) -> int:
-    """floor -> which takes a float n as argument and
-    returns the floor of the float.
-
-    Args:
-        n (float): input number
-
-    Returns:
-        int: floor of the input number,
-    """
-    return int(n)
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
