3b52b938f58e05bd230cb51ae89679c6478adac2
abrhamgg <abrhamgg@gmail.com>
2023-01-05 12:07:24 +0300

    pep8 style

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,7 +4,7 @@ import math
 
 
 def floor(n: float) -> int:
-    """which takes a float n as argument and returns the floor of the float.
+    """floor -> which takes a float n as argument and returns the floor of the float.
 
     Args:
         n (float): input number
