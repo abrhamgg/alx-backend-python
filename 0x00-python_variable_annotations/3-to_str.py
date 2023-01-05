706df59a3025882c158e22315f968ebcbab4b359
abrhamgg <abrhamgg@gmail.com>
2023-01-05 10:41:40 +0300

    3-to_str completed

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""float to string"""
+
+
+def to_str(n: float) -> str:
+    """to_str -> that takes a float n as argument and
+    returns the string representation of the float.
+
+    Args:
+        n (float): input number
+
+    Returns:
+        str: string representation of input number
+    """
+    return str(n)
