dce8e7e3dcb1d6b20cbf2c89a5dde3fdb4d7c7a9
abrhamgg <abrhamgg@gmail.com>
2023-01-05 10:16:23 +0300

    task 0-add.py completed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""add module
+"""
+
+def add(a: float, b:float) -> float:
+    """function that adds two numbers and returns a float
+
+    Args:
+        a (float): number1
+        b (float): number2
+
+    Returns:
+        float: float value
+    """
+    return a + b
