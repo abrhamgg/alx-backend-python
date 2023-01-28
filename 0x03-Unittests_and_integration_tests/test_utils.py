9633bd10bda507e513e5821c609edf79b3f52875
abrhamgg <abrhamgg@gmail.com>
2023-01-28 03:56:53 +0300

    completed task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,14 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Union[Dict, int]) -> None:
         """test for access nested map function"""
         self.assertEqual(access_nested_map(nested_map, sequence), expected)
+
+    @parameterized.expand([
+        ({}, ("a", ), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Tuple[str],
+                                         exception: Exception):
+        """tests the access nested map function"""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
