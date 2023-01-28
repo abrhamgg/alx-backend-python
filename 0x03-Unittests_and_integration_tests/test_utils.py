23016031ae3236c621392eff99226b9371311a06
abrhamgg <abrhamgg@gmail.com>
2023-01-28 03:35:23 +0300

    completed task-0 parametrized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """test utils module"""
 import unittest
-from typing import Mapping, Sequence, Any
+from typing import Mapping, Sequence, Dict, Union, Tuple
 from parameterized import parameterized
 from utils import access_nested_map
-from nose.tools import assert_equal
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,6 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map: Mapping,
-                               sequence: Sequence, expected: Any):
+                               sequence: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
         """test for access nested map function"""
-        assert_equal(access_nested_map(nested_map, sequence), expected)
+        self.assertEqual(access_nested_map(nested_map, sequence), expected)
